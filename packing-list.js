753adb6228844e834d486f6f07fb77889de39c33
Maryan ibrahim <53503272+maryanibrahim@users.noreply.github.com>
2023-05-04 07:20:33 -0500

    packing-list.js

--- packing-list.js
+++ packing-list.js
@@ -1,4 +1,12 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+//Iterating Over an Array With for
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+//Iterating Over an Array With while
 console.log("Kitchen stuff to pack:");
 let i = 0;
 while (i < packingList.length) {
